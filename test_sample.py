2da9054aec75c4fb1844573e91dbfd6f0365a4bb
kiddicoder <112619913+kiddicoder@users.noreply.github.com>
2023-02-25 15:04:32 +0000

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,11 +1,28 @@
-def test_single_item_no_tax_or_shipping():
-    items = [("apple", 1, 0.50)]
-    location = "NY"
-    assert calculate_total(items, location) == 0.50
+import unittest
 
+class Tests(unittest.TestCase):
+    
+    def test_single_item_no_taxes_or_shipping_fees(self, item, location):
+        total_cost = calculate_total_cost(item, location)
+        self.assertEqual(total_cost, 1)
+    
+    def test_multiple_items_no_taxes_or_shipping_fees(self, items, location):
 
-def calculate_total(items, location):
-    total_cost = 0
-    for item in items:
-        total_cost += item[1] * item[2]
-    return total_cost
+        total_cost = calculate_total_cost(items, location)
+        self.assertEqual(total_cost, 2)
+    
+    def test_calculate_taxes(self, items, location):
+        taxes = calculate_taxes(items, location)
+        self.assertEqual(taxes, 1)
+    
+    def test_calculate_shipping_fees(self, items, location):
+        total_weight = calculate_total_weight(items)
+        shipping_fees = calculate_shipping_fees(location, total_weight)
+        self.assertEqual(shipping_fees, 10)
+    
+    def test_total_cost_with_taxes_and_shipping_fees(self, items, location):
+        total_cost = calculate_total_cost(items, location)
+        self.assertEqual(total_cost, 30)
+
+if __name__ == '__main__':
+    unittest.main()
